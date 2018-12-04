15486f3e6fe34e39f284bfdab8fb7fff91155df8
hhh <www.@1603830072.pp.com>
2018-12-04 10:36:08 +0800

    zt-first

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,19 +1,52 @@
-import React,{Component} from "react"
-import css from "./index.module.scss"
+import React,{Component} from "react";
+import axios from "axios";
 import {NavLink} from "react-router-dom";
+import css from "./index.module.scss";
 
 class Home extends Component{
+	constructor(props) {
+	  super(props);
+
+	  this.state = {
+	  	isScroll: false
+	  }
+	}
 	render(){
-		return <div className={css.sss}>
-		<ul>
-			<li><NavLink to="/home/recommend">推荐 </NavLink></li>
-			<li><NavLink to="/home/women">女士 </NavLink></li>
-			<li><NavLink to="/home/men">男士 </NavLink></li>
-			<li><NavLink to="/home/lifestyle">家居 </NavLink></li>
-			<li><NavLink to="/home/kids">婴童 </NavLink></li>
-		</ul>
-		{this.props.children}
-		</div>
+	  return <div className={css.home}>
+	  	<div className={this.state.isScroll?css.scrollHeader:css.header}>
+	  	  <div className={css.topnav+" clear"}>
+	  	    <span className="l"><NavLink to="/login">登录</NavLink></span>
+	  	    <input className={css.input+" l"} type="text" placeholder="PINKO 全场两折起"/>
+	  	    <i className={"iconfont icon-bags l "+css.cart}><NavLink to="/register"></NavLink></i>
+	  	  </div>
+	      <ul className={css.nav}>
+	  	    <li className={css.li+" l"}><NavLink to="/home/recommend">推荐 </NavLink></li>
+	  	    <li className={css.li+" l"}><NavLink to="/home/women">女士 </NavLink></li>
+	  	    <li className={css.li+" l"}><NavLink to="/home/men">男士 </NavLink></li>
+	  	    <li className={css.li+" l"}><NavLink to="/home/lifestyle">家居 </NavLink></li>
+	  	    <li className={css.li+" l"}><NavLink to="/home/kids">婴童 </NavLink></li>
+	      </ul>
+	  	</div>
+	    {this.props.children}
+	  </div>
+	}
+
+	componentDidMount() {
+	  window.onscroll = this.scroll.bind(this);
+	}
+	conponentWillUnmount() {
+	  window.onscroll = null;
+	}
+	scroll() {
+	  if((document.documentElement.scrollTop||document.body.scrollTop)>0) {
+		this.setState({
+			isScroll: true
+		})
+	  } else {
+	  	this.setState({
+			isScroll: false
+		})
+	  }
 	}
 }
 
--- src/components/home/recommend/index.js
+++ src/components/home/recommend/index.js
@@ -1,9 +1,12 @@
-import React,{Component} from "react"
-import css from "./index.module.scss"
+import axios from "axios";
+import React,{Component} from "react";
+import css from "./index.module.scss";
+
 class Recommend extends Component{
 	render(){
-		return <div className={css.sss}>Recommend</div>
+	  return <div className={css.sss}>Recommend</div>
 	}
+
 }
 
 export default Recommend
