48cdb4a23be3be8558180092ef7c5dc3b812b22e
xukun <243886820@qq.com>
2018-12-05 15:12:04 +0800

    xk-3

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -7,23 +7,26 @@ class Login extends Component{
 	  super(props);
 	
 	  this.state = {
-	  	isShow:false
+	  	isShow:false,
+	  	message:'用户名密码不匹配'
 	  };
 	}
 	render(){
-		return <div>
-			<h1>登陆</h1>
-			<i className="iconfont icon-close"></i>
-			<p><NavLink to="/register">去注册</NavLink></p>
-			<div>
-				<input type="text" placeholder="用户名" name="username" ref="username"></input>
+		return <div className={css.box}>
+			<NavLink to="/home"><i className={"iconfont icon-close "+ css.btn}></i></NavLink>
+			<h1  className={css.registertitle}>登陆</h1>
+			<p className={css.smalltitle}><NavLink to="/register">去注册</NavLink></p>
+			<div className={css.registerbox}>
+				<div>
+					<input type="text" placeholder="用户名" name="username" ref="username"></input>
+				</div>
+				<div>
+					<input type="password" placeholder="密码" name="password" ref="password"></input>
+				</div>
+				<button className={css.registerBtn} type="submit" onClick={this.handlClick.bind(this)}>登陆</button>
 			</div>
-			<div>
-				<input type="password" placeholder="密码" name="password" ref="password"></input>
-			</div>
-			<button type="submit" onClick={this.handlClick.bind(this)}>登陆</button>
 			{
-				this.state.isShow?<p>用户名密码不匹配</p>:null
+				this.state.isShow?<p className={css.message}>{this.state.message}</p>:null
 			}
 			
 		</div>
@@ -31,19 +34,30 @@ class Login extends Component{
 	handlClick(){
 		var username = this.refs.username.value;
 		var password = this.refs.password.value;
-		axios.post("/v4/login",{
-			username:username,
-			password:password
-		}).then(res=>{
-			console.log(res.data.state)
-			if(res.data.state === 1){
-				this.props.history.push('/home')
-			}else{
-				this.setState({
-					isShow:true
-				})
-			}
-		})
+		if(username===''|| password===''){
+			this.setState({
+						isShow:true,
+						message:'用户名密码不能为空'
+					})
+			return
+		}else{
+			
+			axios.post("/v4/login",{
+				username:username,
+				password:password
+			}).then(res=>{
+				console.log(res.data.state)
+				if(res.data.state === 1){
+					this.props.history.push('/home')
+				}else{
+					this.setState({
+						isShow:true,
+						message:'用户名密码不匹配'
+					})
+				}
+			})
+		}
+		
 	}
 }
 
--- src/components/register/index.js
+++ src/components/register/index.js
@@ -3,18 +3,31 @@ import css from "./index.module.scss"
 import {NavLink} from "react-router-dom"
 import axios from "axios"
 class Register extends Component{
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	isShow:false,
+	  	message:'用户名已存在'
+	  };
+	}
 	render(){
-		return <div>
-			<h1>注册</h1>
-			<i className="iconfont icon-close"></i>
-			<p><NavLink to="/login">去登陆</NavLink></p>
-			<div>
-				<input type="text" placeholder="用户名" name="username" ref="username"></input>
+		return <div className={css.box}>
+			<NavLink to="/home"><i className={"iconfont icon-close "+ css.btn}></i></NavLink>
+			<h1 className={css.registertitle}>注册</h1>
+			<p className={css.smalltitle}><NavLink to="/login">已有账号 去登陆</NavLink></p>
+			<div className={css.registerbox}>
+				<div>
+					<input type="text" placeholder="用户名" name="username" ref="username"></input>
+				</div>
+				<div>
+					<input type="password" placeholder="密码" name="password" ref="password"></input>
+				</div>
+				<button className={css.registerBtn} type="submit" onClick={this.handlClick.bind(this)}>注册</button>
 			</div>
-			<div>
-				<input type="password" placeholder="密码" name="password" ref="password"></input>
-			</div>
-			<button type="submit" onClick={this.handlClick.bind(this)}>注册</button>
+			{
+				this.state.isShow?<p className={css.message}>{this.state.message}</p>:null
+			}
 		</div>
 	}
 	handlClick(){
@@ -24,11 +37,13 @@ class Register extends Component{
 			username:username,
 			password:password
 		}).then(res=>{
-			
+			console.log(res.data.state);
 			if(res.data.state===1){
 				this.props.history.push('/login')
 			}else{
-				alert("注册失败")
+				this.setState({
+					isShow:true
+				})
 			}
 		})
 	}
