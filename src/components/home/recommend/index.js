955f0dd99f5fb335125bc236ab251b3ecc667b82
hhh <www.@1603830072.pp.com>
2018-12-05 10:01:41 +0800

    zt-seventh

--- src/components/home/recommend/index.js
+++ src/components/home/recommend/index.js
@@ -30,7 +30,7 @@ class Recommend extends Component{
 		  	  <h2 className={css.h2}>{item.name}</h2>
 		  	  {
 		  	  	item.events.map(info=>
-		  	  	  <li className={css.product} key={item.eventId} onClick={this.jump.bind(this,item.categoryId)}>
+		  	  	  <li className={css.product} key={info.eventId} onClick={this.jump.bind(this,info.eventId)}>
 		  	  	  	<img className={css.productImg} src={info.imageUrl} alt=""/>
 		  	  	  	<p className={css.p1}>{info.englishName}</p>
 		  	  	  	<p className={css.p2}>{info.chineseName}</p>
@@ -74,6 +74,7 @@ class Recommend extends Component{
 	  	  datalist: res.data.lists
 	  	})
 	  })
+	  console.log(this.prop)
 	}
 
 	jump(id) {
