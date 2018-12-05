e9b93c071ed5e043953f458ecf76dc8af98a6bb4
xukun <243886820@qq.com>
2018-12-05 20:51:28 +0800

    xk-4

---
+++ src/components/shoppingcart/header/index.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react"
+import css from "./index.module.scss"
+class Header extends Component{
+	render(){
+		return <div className={css.sss}>Header</div>
+	}
+}
+
+export default Header
--- src/components/shoppingcart/index.js
+++ src/components/shoppingcart/index.js
@@ -1,8 +1,25 @@
 import React,{Component} from "react"
 import css from "./index.module.scss"
+import Header from "./header"
 class Shoppingcart extends Component{
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	isnew:true
+	  };
+	}
 	render(){
-		return <div className={css.sss}>Shoppingcart</div>
+		return <div>
+			<Header></Header>
+			<footer className={css.footerlist}>
+				<div>
+					<label className={css.choice}><input type="checkbox" />  全选</label>
+					<p className={css.money}>总价 :<span>￥0000.00</span></p>
+					<div className={this.state.isnew?css.btn:css.red}>去结算</div>
+				</div>
+			</footer>
+		</div>
 	}
 }
 
